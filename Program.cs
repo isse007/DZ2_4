9707afa509d25c9195344d1a87d69957c1233a86
Simon Isupov <is_se@mail.ru>
2024-03-14 22:40:04 +0300

    5

--- Program.cs
+++ Program.cs
@@ -1,14 +1,6 @@
 ﻿// Задача 4: Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
 
-
-// int a = 459;
-// int a1 = a / 100;
-// int a2 = a / 10 % 10;
-// int a3 = a % 10;
-// System.Console.WriteLine($"{a1},{a2},{a3}");
-
-
-void FirstDivSecond(int a)
+void FindThreeNumber(int a)
 {
     int a1 = a / 100;
     int a2 = a / 10 % 10;
@@ -18,4 +10,4 @@ void FirstDivSecond(int a)
 System.Console.WriteLine("Введите трехзначное число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-FirstDivSecond(n);
+FindThreeNumber(n);
